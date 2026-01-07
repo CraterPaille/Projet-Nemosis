52172b06dae8d55f571db7384a2b7f587d604538
jijoxP <117239501+jijoxP@users.noreply.github.com>
2026-01-07 15:06:20 +0100

    Revert "Reapply "test1""
    This reverts commit 34c68f8d21802d5497f7b80b0ae80b22369cb8a6.

--- Assets/Script/Option/InputManager.cs
+++ Assets/Script/Option/InputManager.cs
@@ -8,59 +8,35 @@ public class InputManager : MonoBehaviour
     public PlayerControls keyboardControls;
     public PlayerControls gamepadControls;
 
-    // Permet de créer automatiquement l'InputManager s'il n'existe pas
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    private static void InitializeOnLoad()
-    {
-        if (Instance == null)
-        {
-            GameObject go = new GameObject("InputManager");
-            go.AddComponent<InputManager>();
-        }
-    }
-
     private void Awake()
     {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
+        if (Instance != null) { Destroy(gameObject); return; }
         Instance = this;
         DontDestroyOnLoad(gameObject);
 
         keyboardControls = new PlayerControls();
         gamepadControls = new PlayerControls();
 
-        // Active tous les action maps nécessaires
         keyboardControls.Rhytm.Enable();
-        keyboardControls.Gameplay.Enable();
         gamepadControls.Rhytm.Enable();
-        gamepadControls.Gameplay.Enable();
 
         // Charger rebinding sauvegardé
         if (PlayerPrefs.HasKey("rebinds_keyboard"))
             keyboardControls.asset.LoadBindingOverridesFromJson(PlayerPrefs.GetString("rebinds_keyboard"));
         if (PlayerPrefs.HasKey("rebinds_gamepad"))
             gamepadControls.asset.LoadBindingOverridesFromJson(PlayerPrefs.GetString("rebinds_gamepad"));
-
-        Debug.Log("[InputManager] Initialisé automatiquement.");
     }
 
     private void OnEnable()
     {
         keyboardControls?.Rhytm.Enable();
-        keyboardControls?.Gameplay.Enable();
         gamepadControls?.Rhytm.Enable();
-        gamepadControls?.Gameplay.Enable();
     }
 
     private void OnDisable()
     {
         keyboardControls?.Rhytm.Disable();
-        keyboardControls?.Gameplay.Disable();
         gamepadControls?.Rhytm.Disable();
-        gamepadControls?.Gameplay.Disable();
     }
 
     public void SaveRebinds(bool forGamepad = false)
@@ -74,4 +50,8 @@ public class InputManager : MonoBehaviour
             PlayerPrefs.SetString("rebinds_gamepad", gamepadControls.asset.SaveBindingOverridesAsJson());
         }
     }
+
+
+
+
 }
